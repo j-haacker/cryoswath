7041367a4e40f5aac817698f69b2426af576c611
Jan Haacker <j.m.haacker@tudelft.nl>
2024-02-14 13:51:28 +0100

    chore(l2): make functions visible

--- cryoswath/l2.py
+++ cryoswath/l2.py
@@ -10,32 +10,7 @@ import shapely
 from .misc import *
 from . import gis, l1b
 
-
-def limit_filter(data: pd.DataFrame, column: str, limit: float):
-    return data[np.abs(data[column])<limit]
-
-
-def from_processed_l1b(l1b_data: l1b.l1b_data|None = None, crs: CRS = CRS.from_epsg(3413), **kwargs) -> gpd.GeoDataFrame:
-    # print(data)
-    if l1b_data is None:
-        print("No data passed.")
-        # ! empty GeoDataFrames can't have a CRS
-        # super().__init__(crs=crs, **kwargs)
-        pass
-    else:
-        if isinstance(crs, int): crs = CRS.from_epsg(crs)
-        # "flatten" data to avoid dim order confusion and convert to dataframe
-        l1b_data = l1b_data.to_dataframe().dropna(axis=0, how="any").rename_axis(("time", "sample"))
-        l1b_data.index = l1b_data.index.set_levels(pd.DatetimeIndex(l1b_data["time"].groupby(level=0).first(), tz="UTC"), level=0)
-        l1b_data.drop(columns="time", inplace=True)
-        # convert either lat, lon or x, y data to points assuming that any crs but 4326 uses x, y coordinates
-        if crs == CRS.from_epsg(4326):
-            geometry = gpd.points_from_xy(l1b_data.lon, l1b_data.lat)
-            l1b_data.drop(columns=["lat", "lon"], inplace=True)
-        else:
-            geometry = gpd.points_from_xy(l1b_data.x, l1b_data.y)
-            l1b_data.drop(columns=["x", "y"], inplace=True)
-        return gpd.GeoDataFrame(l1b_data, geometry=geometry, crs=crs, **kwargs)
+__all__ = list()
 
 
 def from_id(track_idx: pd.DatetimeIndex|str,
@@ -74,6 +49,36 @@ def from_id(track_idx: pd.DatetimeIndex|str,
                 l2_buffer.to_feather(os.path.join(current_L2_base_path, cs_full_file_names.loc[current_track_idx]+".feather"))
                 # print("done processing")
     return pd.concat(l2_list)
+__all__.append("from_id")
+
+
+def from_processed_l1b(l1b_data: l1b.l1b_data|None = None, crs: CRS = CRS.from_epsg(3413), **kwargs) -> gpd.GeoDataFrame:
+    # print(data)
+    if l1b_data is None:
+        print("No data passed.")
+        # ! empty GeoDataFrames can't have a CRS
+        # super().__init__(crs=crs, **kwargs)
+        pass
+    else:
+        if isinstance(crs, int): crs = CRS.from_epsg(crs)
+        # "flatten" data to avoid dim order confusion and convert to dataframe
+        l1b_data = l1b_data.to_dataframe().dropna(axis=0, how="any").rename_axis(("time", "sample"))
+        l1b_data.index = l1b_data.index.set_levels(pd.DatetimeIndex(l1b_data["time"].groupby(level=0).first(), tz="UTC"), level=0)
+        l1b_data.drop(columns="time", inplace=True)
+        # convert either lat, lon or x, y data to points assuming that any crs but 4326 uses x, y coordinates
+        if crs == CRS.from_epsg(4326):
+            geometry = gpd.points_from_xy(l1b_data.lon, l1b_data.lat)
+            l1b_data.drop(columns=["lat", "lon"], inplace=True)
+        else:
+            geometry = gpd.points_from_xy(l1b_data.x, l1b_data.y)
+            l1b_data.drop(columns=["x", "y"], inplace=True)
+        return gpd.GeoDataFrame(l1b_data, geometry=geometry, crs=crs, **kwargs)
+__all__.append("from_processed_l1b")
+
+
+def limit_filter(data: pd.DataFrame, column: str, limit: float):
+    return data[np.abs(data[column])<limit]
+__all__.append("limit_filter")
 
 
 def process_and_save(region_of_interest: str|shapely.Polygon,
@@ -94,3 +99,4 @@ def process_and_save(region_of_interest: str|shapely.Polygon,
     from_id(cs_tracks.index)
     print("processing L1b -> L2 finished")
     return 0
+__all__.append("process_and_save")
