234b893ad602a1bca24315f3da51d680cd657dcf
Jan Haacker <j.m.haacker@tudelft.nl>
2024-02-20 08:54:06 +0100

    refactor(misc): change loading RGI shapes
    May require changes in other modules!

--- cryoswath/misc.py
+++ cryoswath/misc.py
@@ -17,6 +17,15 @@ import xarray as xr
 
 from . import gis
 
+# make contents accessible
+__all__ = ["aux_path", "data_path", "dem_path", "cs_ground_tracks_path", "rgi_path", # paths ..........................
+           "WGS84_ellpsoid", "antenna_baseline", "Ku_band_freq", "sample_width",     # vars ...........................
+           "speed_of_light",
+           "cs_id_to_time", "cs_time_to_id", "find_region_id", "flag_translator",    # funcs ..........................
+           "gauss_filter_DataArray", "get_dem_reader", "load_cs_full_file_names", 
+           "load_cs_ground_tracks", "load_o1region", "load_o2region", "load_basins",
+          ]
+
 config = configparser.ConfigParser()
 config_path = os.path.join(os.path.dirname(__file__), "config.ini")
 if os.path.isfile(config_path):
@@ -174,76 +183,88 @@ def get_dem_reader(data: any = None) -> rasterio.DatasetReader:
     return rasterio.open(os.path.join(dem_path, "arcticdem_mosaic_100m_v4.1_dem.tif"))
 
 
-def load_cs_full_file_names(update: bool = False) -> pd.Series:
+def load_cs_full_file_names(update: str = "regular") -> pd.Series:
+    # update one of: "no", "quick", "regular", "full"
     file_names_path = os.path.join(aux_path, "CryoSat-2_SARIn_file_names.pkl")
-    if update:
+    if os.path.isfile(file_names_path):
         file_names = pd.read_pickle(file_names_path).sort_index()
+    if update == "no":
+        return file_names
+    if update == "quick":
+        last_lta_idx = file_names.index[-1]
+        print(last_lta_idx+pd.offsets.MonthBegin(-1, normalize=True))
+    if update == "regular":
+        # ! "regular" should also be baseline and version aware
         last_lta_idx = file_names[(fn[3:7]=="LTA_" for fn in file_names)].index[-1]
         print(last_lta_idx+pd.offsets.MonthBegin(-1, normalize=True))
-        with ftplib.FTP("science-pds.cryosat.esa.int") as ftp:
-            ftp.login(passwd="your@email.address")
-            ftp.cwd("/SIR_SIN_L1")
-            for year in ftp.nlst():
-                if year < str(last_lta_idx.year):
-                    print("skip", year)
-                    continue
-                try:
-                    ftp.cwd(f"/SIR_SIN_L1/{year}")
-                    print(f"entered /SIR_SIN_L1/{year}")
-                    for month in ftp.nlst():
-                        if pd.to_datetime(f"{year}-{month}") < last_lta_idx+pd.offsets.MonthBegin(-1, normalize=True):
-                            print("skip", month)
-                            continue
-                        ftp.cwd(f"/SIR_SIN_L1/{year}/{month}")
-                        print(f"scanning /SIR_SIN_L1/{year}/{month}")
-                        for remote_file in ftp.nlst():
-                            if remote_file[-3:] == ".nc":
-                                remote_idx = pd.to_datetime(remote_file[19:34])
-                                if remote_idx in file_names.index \
-                                and (file_names.loc[remote_idx][3:7]=="LTA_" or remote_file[3:7]=="OFFL"):
-                                    continue
-                                file_names.loc[remote_idx] = remote_file[:-3]
-                except (KeyboardInterrupt, Exception) as err:
-                    if isinstance(err, KeyboardInterrupt):
-                        file_names.to_pickle(file_names_path)
-                        raise
-                    warnings.warn(f"Error occurred in remote directory /SIR_SIN_L1/{year}/{month}.")
-    elif os.path.isfile(file_names_path):
-        return pd.read_pickle(file_names_path)
-    else:
-        file_names = pd.Series(name="cs_full_file_names")
-        with ftplib.FTP("science-pds.cryosat.esa.int") as ftp:
-            ftp.login(passwd="your@email.address")
-            ftp.cwd("/SIR_SIN_L1")
-            for year in ftp.nlst():
-                try:
-                    ftp.cwd(f"/SIR_SIN_L1/{year}")
-                    print(f"entered /SIR_SIN_L1/{year}")
-                    for month in ftp.nlst():
-                        ftp.cwd(f"/SIR_SIN_L1/{year}/{month}")
-                        print(f"scanning /SIR_SIN_L1/{year}/{month}")
-                        for remote_file in ftp.nlst():
-                            if remote_file[-3:] == ".nc":
-                                file_names.loc[pd.to_datetime(remote_file[19:34])] = remote_file[:-3]
-                except (KeyboardInterrupt, Exception) as err:
-                    if isinstance(err, KeyboardInterrupt):
-                        file_names.to_pickle(file_names_path)
-                        raise
-                    warnings.warn(f"Error occurred in remote directory /SIR_SIN_L1/{year}/{month}.")
-    file_names.to_pickle(file_names_path)
-    return file_names
+    with ftplib.FTP("science-pds.cryosat.esa.int") as ftp:
+        ftp.login(passwd=personal_email)
+        ftp.cwd("/SIR_SIN_L1")
+        for year in ftp.nlst():
+            if update != "full" and year < str(last_lta_idx.year):
+                print("skip", year)
+                continue
+            try:
+                ftp.cwd(f"/SIR_SIN_L1/{year}")
+                print(f"entered /SIR_SIN_L1/{year}")
+                for month in ftp.nlst():
+                    if update != "full" and pd.to_datetime(f"{year}-{month}") \
+                                            < last_lta_idx+pd.offsets.MonthBegin(-1, normalize=True):
+                        print("skip", month)
+                        continue
+                    print(f"cwd /SIR_SIN_L1/{year}/{month}")
+                    ftp.cwd(f"/SIR_SIN_L1/{year}/{month}")
+                    print(f"scanning /SIR_SIN_L1/{year}/{month}")
+                    for remote_file in ftp.nlst():
+                        if remote_file[-3:] == ".nc":
+                            remote_idx = pd.to_datetime(remote_file[19:34])
+                            if update == "regular" and remote_idx in file_names.index \
+                                    and (file_names.loc[remote_idx][3:7]=="LTA_" or remote_file[3:7]=="OFFL"):
+                                continue
+                            file_names.loc[remote_idx] = remote_file[:-3]
+            except Exception:
+                warnings.warn(f"Error occurred in remote directory /SIR_SIN_L1/{year}/{month}.")
+            finally:
+                file_names.to_pickle(file_names_path)
+                return file_names
 
 
-def load_cs_ground_tracks() -> gpd.GeoDataFrame:
+def load_cs_ground_tracks(region_of_interest: shapely.Polygon,
+                          start_datetime: pd.Timestamp,
+                          end_datetime: pd.Timestamp, *,
+                          buffer_period_by: relativedelta = None,
+                          buffer_region_by: float = None,
+                          ) -> gpd.GeoDataFrame:
+    start_datetime, end_datetime = pd.to_datetime([start_datetime, end_datetime])
     cs_tracks = gpd.read_feather(cs_ground_tracks_path).set_index("index").sort_index()
     cs_tracks.index = pd.to_datetime(cs_tracks.index)
+    if buffer_period_by is not None:
+        start_datetime = start_datetime - buffer_period_by
+        end_datetime = end_datetime + buffer_period_by
+    cs_tracks = cs_tracks.loc[start_datetime:end_datetime+pd.offsets.Day(1)]
+    if isinstance(region_of_interest, str):
+        region_of_interest = load_glacier_outlines(region_of_interest)
+    if buffer_region_by is not None:
+        region_of_interest = gis.buffer_4326_shp(region_of_interest, buffer_region_by)
+    region_of_interest = gis.simplify_4326_shp(region_of_interest)
+    # find all tracks that intersect the buffered region of interest.
+    # mind that this are calculations on a sphere. currently, the
+    # polygon is transformed to ellipsoidal coordinates. not a 100 %
+    # sure that this doesn't raise issues close to the poles.
+    cs_tracks = cs_tracks[cs_tracks.intersects(region_of_interest)]
     return cs_tracks.set_crs(4326)
 
 
-def load_o1region(o1code: str) -> gpd.GeoDataFrame:
+def load_o1region(o1code: str, product: str = "complexes") -> gpd.GeoDataFrame:
+    if product == "complexes":
+        product = "C"
+    elif product == "glaciers":
+        product = "G"
+    else:
+        raise ValueError(f"Argument product should be either glaciers or complexes not \"{product}\".")
     rgi_files = os.listdir(rgi_path)
     for file in rgi_files:
-        if re.match(f"RGI2000-v7\.0-G-{o1code}_.*", file):
+        if re.match(f"RGI2000-v7\.0-{product}-{o1code[:2]}_.*", file):
             file_path = os.path.join(rgi_path, file)
             if file[-8:] == ".feather":
                 o1region = gpd.read_feather(file_path)
@@ -253,29 +274,50 @@ def load_o1region(o1code: str) -> gpd.GeoDataFrame:
                 continue
             break
     if "o1region" not in locals():
-        print("Make sure RGI files are available in data/auxiliary/RGI. If you did not download them already, you can find them at https://daacdata.apps.nsidc.org/pub/DATASETS/nsidc0770_rgi_v7/regional_files/RGI2000-v7.0-G/. Mind that you need to unzip them. If you decide to put them into a directory, name it as the file is named (e.g. RGI2000-v7.0-G-01_alaska).")
+        print("Make sure RGI files are available in data/auxiliary/RGI. If",
+              "you did not download them already, you can find them at",
+              f"https://daacdata.apps.nsidc.org/pub/DATASETS/nsidc0770_rgi_v7/regional_files/RGI2000-v7.0-{product}/.",
+              "Mind that you need to unzip them. If you decide to put them",
+              "into a directory, name it as the file is named (e.g. RGI2000-v7.0-G-01_alaska).")
         raise FileNotFoundError
+    if product == "C":
+        # ! work-around: drop small glaciers
+        # issue: takes long to do computations or kernel crashes if
+        # (assumption) region contains too many small glaciers. this is
+        # equally true for o2 regions, which is why I drop them here
+        # already. observed for the Alps.
+        small_glacier_mask = o1region.area_km2 < 1
+        if sum(small_glacier_mask) != 0:
+            warnings.warn(f"Dropping {sum(small_glacier_mask)} glaciers < 1 km².")
+        o1region = o1region[~small_glacier_mask]
     return o1region
 
 
-def load_o2region(o2code: str) -> gpd.GeoDataFrame:
-    o1region = load_o1region(o2code[:2])
-    return o1region[o1region["o2region"]==o2code]
+def load_o2region(o2code: str, product: str = "complexes") -> gpd.GeoDataFrame:
+    o1region = load_o1region(o2code[:2], product)
+    return o1region[o1region["o2region"]==o2code[:5]]
 
 
 def load_basins(rgi_ids: list[str]) -> gpd.GeoDataFrame:
     if len(rgi_ids) > 1:
         assert(all([id[:17]==rgi_ids[0][:17]] for id in rgi_ids))
-    rgi_o1_gpdf = load_o1region(rgi_ids[0][15:17])
+    rgi_o1_gpdf = load_o1region(rgi_ids[0].split("-")[3], product="glaciers")
     id_to_index_series = pd.Series(data=rgi_o1_gpdf.index, index=rgi_o1_gpdf.rgi_id)
     return rgi_o1_gpdf.loc[id_to_index_series.loc[rgi_ids].values]
 
 
-# make contents accessible
-__all__ = ["aux_path", "data_path", "dem_path", "cs_ground_tracks_path", "rgi_path", # paths ..........................
-           "WGS84_ellpsoid", "antenna_baseline", "Ku_band_freq", "sample_width",     # vars ...........................
-           "speed_of_light",
-           "cs_id_to_time", "cs_time_to_id", "find_region_id", "flag_translator",    # funcs ..........................
-           "gauss_filter_DataArray", "get_dem_reader", "load_cs_full_file_names", 
-           "load_cs_ground_tracks", "load_o1region", "load_o2region", "load_basins",
-          ]
+def load_glacier_outlines(identifier: str|list[str]) -> shapely.MultiPolygon:
+    print(identifier, len(identifier))
+    if isinstance(identifier, list):
+        out = load_basins(identifier)
+    elif len(identifier) == (7+4+1+2+5+4) and identifier.split("-")[:3] == ["RGI2000", "v4.1", "G"]:
+        out = load_basins([identifier])
+    # the pattern is rather allowing, set it to "^(-?[012][0-9]){2}(_[a-z]+){1,5}(_[0-9][a-z][0-9]?)?$" to make it tight
+    elif len(identifier) >= 5 and re.match("^(-?[012][0-9]){2}$", identifier[:5]):
+        out = load_o2region(identifier[:5])
+    elif re.match("[012][0-9](_[a-z]+)+", identifier):
+        out = load_o1region(identifier[:2])
+    else:
+        raise ValueError(f"Provided o1, o2, or RGI identifiers. \"{identifier}\" not understood.")
+    return out.unary_union
+__all__.append("load_glacier_outlines")
