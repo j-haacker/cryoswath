d796b22bb4d10d2ab2e71ed1fe20ef690d1bd59f
Jan Haacker <152862650+j-haacker@users.noreply.github.com>
2024-02-28 16:35:40 +0100

    feat(tests): add power waveform plot; fill glacier transect

--- cryoswath/test_plots/waveform.py
+++ cryoswath/test_plots/waveform.py
@@ -87,6 +87,103 @@ def coherence(waveform, *,
     return ax
 
 
+def power(waveform, *,
+              ax: plt.Axes = None,
+              logarithmic: bool = True,
+              plot_properties: dict[str, dict[str, any]] = \
+                    dict(swath=dict(color="tab:blue",
+                                    marker='.',
+                                    markersize=5,
+                                    linewidth=1),
+                         poca=dict(color="tab:green",
+                                   marker='o',
+                                   markersize=5,
+                                   linewidth=1),
+                         excluded=dict(color="tab:pink",
+                                       marker='x',
+                                       markersize=5,
+                                    #    linestyle="solid",
+                                       linewidth=.6),
+                         omitted=dict(hatch='///',
+                                      linewidth=.4,
+                                      edgecolor="xkcd:light grey",
+                                      facecolor=None),
+                         threshold=dict(hatch='\\\\\\',
+                                        linewidth=.4,
+                                        edgecolor="xkcd:light grey",
+                                        facecolor=None))
+            ):
+    if ax is None:
+        ax = plt.subplots()[1]
+    if logarithmic:
+        waveform["P"] = 10*np.log10(waveform.power_waveform_20_ku)
+    else:
+        waveform["P"] = waveform.power_waveform_20_ku
+    h_smooth = gauss_filter_DataArray(waveform.P, "ns_20_ku", 35, 35).plot(c=".7", label="smooth")[0]
+    h_list = [h_smooth]
+    try:
+        if plot_properties["threshold"]["facecolor"] is None:
+            plot_properties["threshold"]["facecolor"] = ax.get_facecolor()
+        assert(waveform.power_threshold[0] == "snr")
+        if logarithmic:
+            power_threshold = waveform.noise_power_20_ku[0]+10*np.log10(waveform.power_threshold[1])
+            y0 = -250
+        else:
+            power_threshold = waveform.noise_power_20_ku[0]*waveform.power_threshold[1]
+            y0 = -1
+        h_thr = ax.add_patch(mpl.patches.Rectangle((-100, y0),
+                                                     waveform.ns_20_ku[-1]+200, power_threshold-y0,
+                                                     **plot_properties["threshold"], label="threshold"))
+        h_list.append(h_thr)
+    except KeyError:
+        pass
+    try:
+        if waveform.swath_start[0] > 0:
+            if plot_properties["omitted"]["facecolor"] is None:
+                plot_properties["omitted"]["facecolor"] = ax.get_facecolor()
+            x0 = -100
+            h_omitted = ax.add_patch(mpl.patches.Rectangle((x0, -250),
+                                                        waveform.swath_start[0]-x0, 252,
+                                                        **plot_properties["omitted"], label="omitted"))
+            h_list.insert(1, h_omitted)
+    except AttributeError:
+        pass
+    try:
+        excluded = waveform.where(waveform.exclude_mask)
+        h_excl, = ax.plot(excluded.ns_20_ku, excluded.P[0], ls='',
+                          **plot_properties["excluded"], label="excluded")
+        h_list.insert(0, h_excl)
+        swath = waveform.where(~waveform.exclude_mask)
+        h_swath, = ax.plot(swath.ns_20_ku, swath.P[0], ls='', **plot_properties["swath"],
+                           label="swath")
+        h_list.insert(0, h_swath)
+    except KeyError:
+        h_all, = ax.plot(waveform.ns_20_ku, waveform.P[0], ls='', **plot_properties["swath"],
+                         label="all")
+        h_list.insert(0, h_all)
+    try:
+        poca = waveform.sel(ns_20_ku=waveform.poca_idx)
+        h_poca, = ax.plot(poca.ns_20_ku, poca.P[0], ls='', **plot_properties["poca"],
+                          label="POCA")
+        h_list.insert(0, h_poca)
+    except KeyError:
+        pass
+    ax.set_xlim([waveform.ns_20_ku[0], waveform.ns_20_ku[-1]])
+    if logarithmic:
+        ax.set_ylim([-200, -100])
+    else:
+        ax.set_ylim([1e-20, 1e-10])
+    ax.set_xlabel("sample number (ns_20_ku)")
+    if logarithmic:
+        # note that the decibel, here, refers to 1 watt
+        ax.set_ylabel("power, decibel")
+    else:
+        ax.set_ylabel("power, watt")
+    ax.legend(handles=h_list)
+    ax.set_title(f"id: {waveform.time_20_ku.values[0]}")
+    return ax
+
+
 def dem_transect(waveform, *,
                  ax: plt.Axes = None,
                  line_properties: dict[str, dict[str, any]] = \
@@ -104,7 +201,8 @@ def dem_transect(waveform, *,
                                        linewidth=1),
                          dem=dict(color="black",
                                   linestyle="solid",
-                                  linewidth=0.6)
+                                  linewidth=0.6,
+                                  facecolor="xkcd:ice")
                     ),
                  selected_phase_only: bool = True):
     if ax is None:
@@ -120,7 +218,8 @@ def dem_transect(waveform, *,
     xs, ys = trans_4326_to_dem_crs.transform(lats, lons)
     ref_elevs = np.fromiter(dem_reader.sample([(x, y) for x, y in zip(xs, ys)]), "float32")
     ref_elevs = np.where(ref_elevs!=dem_reader.nodata, ref_elevs, np.nan)
-    h_dem, = ax.plot(sampling_dist, ref_elevs, **line_properties["dem"], label="DEM")
+    ax.fill_between(sampling_dist, ref_elevs, **line_properties["dem"], label="DEM")
+    h_dem = ax.get_legend_handles_labels()[0][0]
     if not selected_phase_only:
         for ph_idx in waveform.phase_wrap_factor.values:
             temp = waveform.sel(phase_wrap_factor=ph_idx)
