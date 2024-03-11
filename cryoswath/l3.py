7e899446633bb1fe6c20f4cea6a02ac47bf3792d
Jan Haacker <152862650+j-haacker@users.noreply.github.com>
2024-03-11 10:44:39 +0100

    feat(L3)!: switch to using dask
    The implemented routine acts on groups of months as independent entities. While on could believe using a more pandas-native approach like resample, this seems not to be the case; probably because the data I used was date-sorted, such that the data of each grid cell is scattered throughout the entire input dataset.

--- cryoswath/l3.py
+++ cryoswath/l3.py
@@ -1,5 +1,6 @@
+import dask.dataframe
 from dateutil.relativedelta import relativedelta
-import geopandas as gpd
+# import numba
 import numpy as np
 import os
 import pandas as pd
@@ -9,56 +10,109 @@ from . import l2
 from .misc import *
 
 __all__ = list()
+    
+
+# numba does not do help here easily. using the numpy functions is as fast as it gets.
+def med_iqr_cnt(data):
+    quartiles = np.quantile(data, [.25, .5, .75])
+    return pd.DataFrame([[quartiles[1], quartiles[2]-quartiles[0], len(data)]], columns=["_median", "_iqr", "_count"])
+__all__.append("med_iqr_cnt")
 
 
 def build_dataset(region_of_interest: str|shapely.Polygon,
                   start_datetime: str|pd.Timestamp,
                   end_datetime: str|pd.Timestamp, *,
-                  aggregation_period: relativedelta = relativedelta(months=3),
-                  timestep: relativedelta = relativedelta(months=1),
+                  l2_type: str = "swath",
+                  timestep_months: int = 1,
+                  window_ntimesteps: int = 3,
                   spatial_res_meter: float = 500,
-                  **kwargs):
+                  agg_func_and_meta: tuple[callable, dict] = (med_iqr_cnt,
+                                                              {"_median": "f8", "_iqr": "f8", "_count": "i8"}),
+                  **l2_from_id_kwargs):
+    if window_ntimesteps%2 - 1:
+        old_window = window_ntimesteps
+        window_ntimesteps = (window_ntimesteps//2+1)
+        warnings.warn(f"The window should be a uneven number of time steps. You asked for {old_window}, but it has "+ f"been changed to {window_ntimesteps}.")
+    # ! end time step should be included.
     start_datetime, end_datetime = pd.to_datetime([start_datetime, end_datetime])
-    print("Building a gridded dataset of elevation estimates for the region",
-          f"{region_of_interest} from {start_datetime} to {end_datetime} for",
-          f"a rolling window of {aggregation_period} every {timestep}.")
-    # if len(aggregation_period.kwds.keys()) != 1 \
-    # or len(timestep.kwds.keys()) != 1 \
-    # or list(aggregation_period.kwds.keys())[0] not in ["years", "months", "days"] \
-    # or list(timestep.kwds.keys())[0] not in ["years", "months", "days"]:
-    #     raise Exception("Only use one of years, months, days for agg_time and timestep.")
+    print("Building a gridded dataset of elevation estimates for",
+          "the region "+region_of_interest if isinstance(region_of_interest, str) else "a custom area",
+          f"from {start_datetime} to {end_datetime} every {timestep_months} for",
+          f"a rolling window of {window_ntimesteps} time steps.")
     if "buffer_region_by" not in locals():
         # buffer_by defaults to 30 km to not miss any tracks. Usually,
         # 10 km should do.
         buffer_region_by = 30_000
-    time_buffer = (aggregation_period-timestep)/2
+    time_buffer_months = (window_ntimesteps*timestep_months)//2
+    ext_t_axis = pd.date_range(start_datetime-pd.DateOffset(months=time_buffer_months),
+                               end_datetime+pd.DateOffset(months=time_buffer_months),
+                               freq=f"{timestep_months}MS",
+                               ).astype("int64")
     cs_tracks = load_cs_ground_tracks(region_of_interest, start_datetime, end_datetime,
-                                      buffer_period_by=time_buffer,buffer_region_by=buffer_region_by)
+                                      buffer_period_by=relativedelta(months=time_buffer_months),
+                                      buffer_region_by=buffer_region_by)
     print("First and last available ground tracks are on",
           f"{cs_tracks.index[0]} and {cs_tracks.index[-1]}, respectively.,",
-          f"{cs_tracks.shape[0]} tracks in total.")
-    print("Run update_cs_ground_tracks, optionally with `full=True` or",
+          f"{cs_tracks.shape[0]} tracks in total."
+          "\n[note] Run update_cs_ground_tracks, optionally with `full=True` or",
           "`incremental=True`, if you local ground tracks store is not up to",
           "date. Consider pulling the latest version from the repository.")
-    # I believe passing loading l2 data to the function prevents copying
-    # on .drop. an alternative would be to define l2_data nonlocal
-    # within the gridding function
-    l3_data =  med_mad_cnt_grid(l2.from_id(cs_tracks.index, **filter_kwargs(l2.from_id, kwargs)),
-                                start_datetime=start_datetime, end_datetime=end_datetime,
-                                aggregation_period=aggregation_period, timestep=timestep,
-                                spatial_res_meter=spatial_res_meter)
-    l3_data.to_netcdf(build_path(region_of_interest, timestep, spatial_res_meter, aggregation_period))
+
+    print("Storing the essential L2 data in hdf5, downloading and",
+          "processing L1b files if not available...")
+    if isinstance(region_of_interest, str):
+        region_id = region_of_interest
+    else:
+        region_id = "_".join([region_of_interest.centroid.x, region_of_interest.centroid.y])
+    cache_path = os.path.join(data_path, "tmp", region_id)
+    l2.from_id(cs_tracks.index, save_or_return="save", cache=cache_path,
+               **filter_kwargs(l2.from_id, l2_from_id_kwargs, blacklist=["save_or_return", "cache"]))
+
+    print("Gridding the data...")
+    # one could drop some of the data before gridding. however, excluding
+    # off-glacier data is expensive and filtering large differences to the
+    # DEM can hide issues while statistics like the median and the IQR
+    # should be fairly robust.
+    l2_ddf = dask.dataframe.read_hdf(cache_path, l2_type, sorted_index=True)
+    l2_ddf = l2_ddf.loc[ext_t_axis[0]:ext_t_axis[-1]]
+    l2_ddf = l2_ddf.repartition(npartitions=3*len(os.sched_getaffinity(0)))
+
+    l2_ddf[["x", "y"]] = l2_ddf[["x", "y"]]//spatial_res_meter*spatial_res_meter
+    l2_ddf["roll_0"] = l2_ddf.index.map_partitions(pd.cut, bins=ext_t_axis, right=False, labels=False, include_lowest=True)
+    for i in range(1, window_ntimesteps):
+        l2_ddf[f"roll_{i}"] = l2_ddf.map_partitions(lambda df: df.roll_0-i).persist()
+    for i in range(window_ntimesteps):
+        l2_ddf[f"roll_{i}"] = l2_ddf[f"roll_{i}"].map_partitions(lambda series: series.astype("i4")//window_ntimesteps)
+
+    roll_res = [None]*window_ntimesteps
+    for i in range(window_ntimesteps):
+        roll_res[i] = l2_ddf.rename(columns={f"roll_{i}": "time_idx"}).groupby(["time_idx", "x", "y"], sort=False).h_diff.apply(agg_func_and_meta[0], meta=agg_func_and_meta[1]).persist()
+    for i in range(window_ntimesteps):
+        roll_res[i] = roll_res[i].compute().droplevel(3, axis=0)
+        roll_res[i].index = roll_res[i].index.set_levels(
+            (roll_res[i].index.levels[0]*window_ntimesteps+i+1), level=0).rename("time", level=0)
+        
+    l3_data = pd.concat(roll_res).sort_index()\
+                                 .loc[(slice(0,len(ext_t_axis)-1),slice(None),slice(None)),:]
+    l3_data.index = l3_data.index.remove_unused_levels()
+    l3_data.index = l3_data.index.set_levels(
+            ext_t_axis[l3_data.index.levels[0]].astype("datetime64[ns]"), level=0)
+    l3_data = l3_data.query(f"time >= '{start_datetime}' and time <= '{end_datetime}'")
+    l3_data.to_xarray().to_netcdf(build_path(region_id, timestep_months, spatial_res_meter))
     return l3_data
 __all__.append("build_dataset")
 
 
-def build_path(region_of_interest, timestep, spatial_res_meter, aggregation_period):
-    region_id = find_region_id(region_of_interest)
-    if list(timestep.kwds.values())[0]!=1:
-        timestep_str = str(list(timestep.kwds.values())[0])+"-"
+def build_path(region_of_interest, timestep_months, spatial_res_meter, aggregation_period):
+    if not isinstance(region_of_interest, str):
+        region_id = find_region_id(region_of_interest)
+    else:
+        region_id = region_of_interest
+    if timestep_months != 1:
+        timestep_str = str(timestep_months)+"-"
     else:
         timestep_str = ""
-    timestep_str += list(timestep.kwds.keys())[0][:-1]+"ly"
+    timestep_str += "monthly"
     if spatial_res_meter == 1000:
         spatial_res_str = "1km"
     elif np.floor(spatial_res_meter/1000) < 2:
@@ -69,29 +123,6 @@ def build_path(region_of_interest, timestep, spatial_res_meter, aggregation_peri
     return os.path.join(data_path, "L3", "_".join(
         [region_id, timestep_str, spatial_res_str+".nc"]))
 __all__.append("build_path")
-    
-
-def med_mad_cnt_grid(l2_data: gpd.GeoDataFrame, *,
-                     start_datetime: pd.Timestamp,
-                     end_datetime: pd.Timestamp,
-                     aggregation_period: relativedelta,
-                     timestep: relativedelta,
-                     spatial_res_meter: float):
-    def stats(data: pd.Series) -> pd.Series:
-        median = data.median()
-        mad = np.abs(data-median).median()
-        return pd.Series([median, mad, data.shape[0]])
-    time_axis = pd.date_range(start_datetime+pd.offsets.MonthBegin(0), end_datetime, freq=timestep)
-    if time_axis.tz == None: time_axis = time_axis.tz_localize("UTC")
-    # if l2_data.index[0].tz == None: l2_data.index = l2_data.index.tz_localize("UTC")
-    def rolling_stats(data):
-        results_list = [None]*aggregation_period.months
-        for i in range(aggregation_period.months):
-            results_list[i] = data.groupby(subset.index.get_level_values("time")-pd.offsets.QuarterBegin(1, normalize=True)+pd.DateOffset(months=i)).apply(stats)
-        result = pd.concat(results_list).unstack().sort_index().rename(columns={0: "med_elev_diff", 1: "mad_elev_diff", 2: "cnt_elev_diff"})#, inplace=True
-        return result.loc[time_axis.join(result.index, how="inner")]
-    return l2.grid(l2_data, spatial_res_meter, rolling_stats).to_xarray()
-__all__.append("med_mad_cnt_grid")
 
 
 __all__ = sorted(__all__)
