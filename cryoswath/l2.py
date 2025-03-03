8d2337a1e89c1db3aab637fe7ede3d7c6cf1b0e1
jan <152862650+j-haacker@users.noreply.github.com>
2025-03-03 21:36:55 +0100

    chore(l2): satisfy flake8

--- cryoswath/l2.py
+++ cryoswath/l2.py
@@ -24,10 +24,19 @@ from tables import NaturalNameWarning
 import warnings
 import xarray as xr
 
-from .misc import *
+from .misc import (
+    cryosat_id_pattern,
+    cs_id_to_time,
+    cs_time_to_id,
+    data_path,
+    filter_kwargs,
+    load_cs_full_file_names,
+    load_cs_ground_tracks,
+    l2_poca_path,
+    l2_swath_path,
+    xycut,
+)
 from . import l1b
-
-__all__ = list()
 
 
 def from_id(
@@ -50,7 +59,7 @@ def from_id(
     if cores is None:
         try:
             cores = len(os.sched_getaffinity(0))
-        except:
+        except AttributeError:
             warnings.warn(
                 'Failed to find number of CPU cores. Set manually using "cores=".'
             )
@@ -60,7 +69,7 @@ def from_id(
             track_idx if isinstance(track_idx, list) else [track_idx]
         )
     track_idx = track_idx.sort_values()
-    if track_idx.tz != None:
+    if track_idx.tz is not None:
         track_idx = track_idx.tz_localize(None)
     if isinstance(reprocess, str):
         reprocess = pd.Timestamp(reprocess)
@@ -75,7 +84,8 @@ def from_id(
     )
     # stop_event = Event()
     # download_thread = Thread(target=l1b.download_wrapper,
-    #                          kwargs=dict(track_idx=track_idx, num_processes=8, stop_event=stop_event),
+    #                          kwargs=dict(track_idx=track_idx, num_processes=8,
+    #                                      stop_event=stop_event),
     #                          name="dl_l1b_mother_thread",
     #                          daemon=False)
     # download_thread.start()
@@ -131,7 +141,9 @@ def from_id(
                 os.makedirs(os.path.join(data_path, f"L2_{l2_type}", current_subdir))
         print("start processing", current_month)
         # # indices per month with work-around :/ should be easier
-        # current_track_indices = pd.Series(index=track_idx).loc[current_month:current_month+pd.offsets.MonthBegin(1)].index
+        # current_track_indices = pd.Series(index=track_idx).loc[
+        #       current_month:current_month+pd.offsets.MonthBegin(1)
+        # ].index
         current_track_indices = track_idx[
             np.logical_and(
                 track_idx >= current_month,
@@ -216,10 +228,10 @@ def from_id(
             # is parquet or the data could be saved per month using
             # to_hdf(format="fixed")
 
-            # in the next step, the data will be cached on disk for later use. this bears
-            # the risk of introducing data gaps that are very difficult to detect.
-            # the main rationale is: be sure not to damage the data and be careful
-            # not to loose the cache.
+            # in the next step, the data will be cached on disk for later use. this
+            # bears the risk of introducing data gaps that are very difficult to
+            # detect. the main rationale is: be sure not to damage the data and be
+            # careful not to loose the cache.
             # this implementation balances the chance loosing the current data with
             # the cost of copying by backing up once per hour
             if os.path.isfile(cache_fullname):
@@ -275,12 +287,13 @@ def from_id(
                             ),
                         )
                         # below, x and y will be stored as integers which rounds them
-                        # implicitly. this introduces a logic-flaw in the chunking, where
-                        # floor-division is used. this makes an extra step necessary. note that
-                        # pandas does not offer `.floor()`. w/out testing, I assume
-                        # `round(x-.5)` to perform better than x//1.
+                        # implicitly. this introduces a logic-flaw in the chunking,
+                        # where floor-division is used. this makes an extra step
+                        # necessary. note that pandas does not offer `.floor()`. w/out
+                        # testing, I assume `round(x-.5)` to perform better than x//1.
                         tmp[["x", "y"]] = (tmp[["x", "y"]] - 0.5).round()
-                        # below hides warnings about a minus sign in node names. this can safely be ignored.
+                        # below hides warnings about a minus sign in node names. this
+                        # can safely be ignored.
                         warnings.filterwarnings("ignore", category=NaturalNameWarning)
                         tmp.astype(
                             dict(h_diff=np.float32, x=np.int32, y=np.int32)
@@ -289,8 +302,20 @@ def from_id(
                             key="/".join(
                                 [
                                     l2_type,
-                                    f'x_{chunk["x_interval_start"]:.0f}_{chunk["x_interval_stop"]:.0f}',
-                                    f'y_{chunk["y_interval_start"]:.0f}_{chunk["y_interval_stop"]:.0f}',
+                                    "_".join(
+                                        [
+                                            "x",
+                                            f'{chunk["x_interval_start"]:.0f}',
+                                            f'{chunk["x_interval_stop"]:.0f}',
+                                        ]
+                                    ),
+                                    "_".join(
+                                        [
+                                            "y",
+                                            f'{chunk["y_interval_start"]:.0f}',
+                                            f'{chunk["y_interval_stop"]:.0f}',
+                                        ]
+                                    ),
                                     current_month.strftime("t_%Y-%m"),
                                 ]
                             ),
@@ -298,17 +323,17 @@ def from_id(
                             format="fixed",
                         )
                         warnings.filterwarnings("default", category=NaturalNameWarning)
-                except:
+                except Exception:
                     if not os.path.isfile(cache_fullname):
                         raise
                     os.remove(cache_fullname)
                     warnings.warn(
-                        "There was an error while caching l2 data. Since this can lead to "
-                        "missing data, the cache has been removed and it was attempted to "
-                        "restore a backup. If this was successful, there should NOT be a "
-                        "__backup file in data/tmp. If there still is, please decide how to "
-                        "proceed yourself (you could remove all temporary files and have "
-                        "them produced freshly)."
+                        "There was an error while caching l2 data. Since this can lead "
+                        "to missing data, the cache has been removed and it was "
+                        "attempted to restore a backup. If this was successful, there "
+                        "should NOT be a __backup file in data/tmp. If there still is, "
+                        "please decide how to proceed yourself (you could remove all "
+                        "temporary files and have them produced freshly)."
                     )
                     shutil.move(cache_fullname + "__backup", cache_fullname)
                     raise
@@ -372,7 +397,8 @@ def from_processed_l1b(
     tmp.drop(
         columns=[col for col in ["time", "sample"] if col in tmp.columns], inplace=True
     )
-    # convert either lat, lon or x, y data to points assuming that any crs but 4326 uses x, y coordinates
+    # convert either lat, lon or x, y data to points assuming that any
+    # crs but 4326 uses x, y coordinates
     if l1b_data.CRS == CRS.from_epsg(4326):
         geometry = gpd.points_from_xy(tmp.lon, tmp.lat)
         tmp.drop(columns=["lat", "lon"], inplace=True)
@@ -484,7 +510,8 @@ def process_track(idx, reprocess, l2_paths, save_or_return, current_subdir, kwar
             raise FileNotFoundError()
         if isinstance(reprocess, pd.Timestamp):
             # print(reprocess, pd.Timestamp(
-            #     os.stat(os.path.join(l2_swath_path, current_subdir, l2_paths.loc[idx, "swath"])).st_mtime,
+            #     os.stat(os.path.join(l2_swath_path, current_subdir,
+            #                          l2_paths.loc[idx, "swath"])).st_mtime,
             #     unit="s"))
             assert reprocess < pd.Timestamp(
                 os.stat(
@@ -534,7 +561,9 @@ def process_track(idx, reprocess, l2_paths, save_or_return, current_subdir, kwar
             # print("debug 2", idx, flush=True)
             # the below is necessary to pass a specific dem file. skipped by default.
             if "dem_file_name_or_path" in kwargs:
-                # tmp = tmp.append_ambiguous_reference_elevation(kwargs.pop("dem_file_name_or_path"))
+                # tmp = tmp.append_ambiguous_reference_elevation(kwargs.pop(
+                #       "dem_file_name_or_path"
+                # ))
                 tmp = tmp.append_ambiguous_reference_elevation(
                     kwargs["dem_file_name_or_path"]
                 )
@@ -549,7 +578,8 @@ def process_track(idx, reprocess, l2_paths, save_or_return, current_subdir, kwar
             else:
                 # raise
                 warnings.warn(
-                    f"Error {str(err)} occured while processing l1b {idx}. Continuing with next file."
+                    f"Error {str(err)} occured while processing l1b {idx}. Continuing "
+                    "with next file."
                 )
                 swath_poca_tuple = (gpd.GeoDataFrame(), gpd.GeoDataFrame())
         if save_or_return != "return":
